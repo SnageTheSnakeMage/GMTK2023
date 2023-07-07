b97d6bd50a5b608ec761c340881a455f7168710e
Bad-At-Making-Names <fbfukasawa@gmail.com>
2023-07-07 18:54:57 -0400

    Finished basic movement for hero

--- Untitled GMTKGameJam2023/Assets/Scripts/heroActions.cs
+++ Untitled GMTKGameJam2023/Assets/Scripts/heroActions.cs
@@ -8,7 +8,8 @@ public class heroActions : MonoBehaviour
     public float moveSpeed = 2;
     public float jumpHeight = 10;
     private bool grounded = true;
-    private bool facingRight = true;
+    public bool facingRight = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,23 +18,63 @@ public class heroActions : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        //Constantly moving forwards
+        //Boolean for if there's a floor in front of the hero
+        bool isFloor;
+
+        //Constantly moving forwards and checking for pits
         if (facingRight)
         {
             myRigidBody.velocity = new Vector2(moveSpeed, myRigidBody.velocity.y);
+            isFloor = Physics2D.Raycast(transform.position + Vector3.right, Vector2.down, 10f);
         }
         else
         {
             myRigidBody.velocity = new Vector2(-moveSpeed, myRigidBody.velocity.y);
+            isFloor = Physics2D.Raycast(transform.position + Vector3.left, Vector2.down, 10f);
         }
-        //Detecting if there's a pit
-        bool isFloor = Physics2D.Raycast(transform.position + Vector3.right, Vector2.down, 10f);
-        if (!isFloor)
+
+        //If there's a gap in the ground and the hero is grounded, jump
+        if (!isFloor && grounded)
         {
             myRigidBody.velocity += new Vector2(0, jumpHeight);
+            grounded = false;
+        }
+
+
+    }
+
+    //Checks what object the hero hits when he collides with something
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        switch(collision.gameObject.tag)
+        {
+            case "Ground":
+                grounded = true;
+                break;
+            case "Wall":
+                turnAround();
+                break;
+            case "Danger":
+                die();
+                break;
         }
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+
+    //Turns the hero around
+    private void turnAround()
+    {
+        if(facingRight)
+        {
+            facingRight = false;
+        }
+        else
+        {
+            facingRight = true;
+        }
+    }
+
+    //Death animation and respawn
+    public void die()
     {
 
     }
