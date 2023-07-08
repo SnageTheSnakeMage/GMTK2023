22513fbc42acec1a06850f7f889bac4daef5ccb2
SnageTheSnakeMage <101869780+SnageTheSnakeMage@users.noreply.github.com>
2023-07-08 11:48:01 -0700

    FInished up drag and drop

---
+++ Untitled GMTKGameJam2023/Assets/blockItem.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blockItem : MonoBehaviour
+{
+    public int iD;
+    private dragAndDropManager dragAndDropMgr;
+    void Start()
+    {
+        dragAndDropMgr = GameObject.FindGameObjectWithTag("dragAndDropManager").GetComponent<dragAndDropManager>();
+    }
+
+    private void OnMouseOver()
+    {
+        if(Input.GetMouseButtonDown(1))
+        {
+            Destroy(this.gameObject);
+            dragAndDropMgr.allItems[iD].quantity++;
+            dragAndDropMgr.allItems[iD].quantityText.text = dragAndDropMgr.allItems[iD].quantity.ToString();
+        }
+    }
+
+}
