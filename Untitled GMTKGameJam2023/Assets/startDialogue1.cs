58f667a609818c9c86ea399019a3bcb17b849b75
Bad-At-Making-Names <fbfukasawa@gmail.com>
2023-07-09 02:11:13 -0400

    Finished intro cutscene + tutorial

--- Untitled GMTKGameJam2023/Assets/startDialogue1.cs
+++ Untitled GMTKGameJam2023/Assets/startDialogue1.cs
@@ -7,11 +7,15 @@ public class startDialogue1 : MonoBehaviour
     public GameObject hero;
     private DialogueTrigger dialogueTrigger;
     public GameObject dialogueBox;
+    public GameObject dialogueBox2;
+    public GameObject buttons;
 
     // Start is called before the first frame update
     void Start()
     {
         hero.SetActive(false);
+        dialogueBox2.SetActive(false);
+        buttons.SetActive(false);
         dialogueTrigger = this.gameObject.GetComponent<DialogueTrigger>();
         dialogueTrigger.triggerDialogue(false);
     }
